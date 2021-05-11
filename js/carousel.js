29ae17a8dbe8abe6a7814bac3518211d35255677
dziugaspeciulevicius <dziugas.peciulevicius@visma.com>
2021-05-11 12:34:48 +0300

    PR Changes: - carousel controls are displayed in the DOM - reducing DOM operations and storing final HTML and passing it into innerHTML - changed up function naming - installing dev-dependencies (http and json servers) into project level instead of keeping them global to increase project portability

--- js/carousel.js
+++ js/carousel.js
@@ -1,71 +1,60 @@
+  
 import { fetchCarouselData } from "./fetch.js";
+const slides = document.querySelector(".slides");
 
-// display carousel categories from database
 window.addEventListener("DOMContentLoaded", async function () {
   let carouselData = await fetchCarouselData();
-  displayCarouselData(carouselData);
+  displayCarousel(carouselData);
   displayControls();
 });
 
-// display carousel data
-const displayCarouselData = (carouselData) => {
-  document.querySelector(".slides").innerHTML = "";
+const displayCarousel = (carouselData) => {
+  const carouselItems = carouselData.map((item) => `
+    <figure class="carousel-container">
+      <a href="#${item.id}">
+        <img src="${item.imageUrl}" alt="slide image" />
+        <figcaption class="carousel-text">${item.title}</figcaption>
+        <figcaption class="carousel-subtext">${item.description}</figcaption>
+      </a>
+    </figure>`).join("");
 
-  // loop through carouselData
-  carouselData.forEach((item) => {
-    document.querySelector(".slides").innerHTML += `
-      <figure class="carousel-container">
-        <a href="#${item.id}">
-          <img src="${item.imageUrl}" alt="slide image" />
-          <figcaption class="carousel-text">${item.title}</figcaption>
-          <figcaption class="carousel-subtext">${item.description}</figcaption>
-        </a>
-      </figure>
-    `;
-  });
-};
+    slides.innerHTML = `
+      ${carouselItems}
+    `
+} 
 
-// display carousel controls
 const displayControls = () => {
-  document.querySelector(".controls").innerHTML = "";
-
-  // display controls
-  document.querySelector(".controls").innerHTML += `
-    <div class="control prev" id=\"prev\"></div>
-    <div class="control next" id=\"next\"></div>
-  `;
-
   document.getElementById("prev").onclick = function () {
-    changeSlide(false);
-    restart();
+    showPrevSlide()
+    restartAutoChange();
   };
 
   document.getElementById("next").onclick = function () {
-    changeSlide();
-    restart();
+    showNextSlide()
+    restartAutoChange();
   };
 
   // CAROUSEL FUNCTIONALITY
   const delay = 5000; //ms
-  const slides = document.querySelector(".slides");
   const slidesCount = slides.childElementCount;
   const maxLeftSlide = (slidesCount - 1) * 100 * -1;
 
   let currentSlide = 0;
 
-  const changeSlide = (next = true) => {
-    if (next) {
-      currentSlide += currentSlide > maxLeftSlide ? -100 : currentSlide * -1;
-    } else {
-      currentSlide = currentSlide < 0 ? currentSlide + 100 : maxLeftSlide;
-    }
-
+  const showNextSlide = () => {
+    currentSlide += currentSlide > maxLeftSlide ? -100 : currentSlide * -1;
     slides.style.left = currentSlide + "%";
-  };
-  let autoChange = setInterval(changeSlide, delay);
+  }
+  
+  const showPrevSlide = () => {
+    currentSlide = currentSlide < 0 ? currentSlide + 100 : maxLeftSlide;
+    slides.style.left = currentSlide + "%";
+  }
 
-  const restart = () => {
+  let autoChange = setInterval(showNextSlide, delay);
+
+  const restartAutoChange = () => {
     clearInterval(autoChange);
-    autoChange = setInterval(changeSlide, delay);
+    autoChange = setInterval(showNextSlide, delay);
   };
 };
